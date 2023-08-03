0be884d64f98c0b20dea5f95f22129b104890206
Alexey <alexeylutv@vk.com>
2023-08-03 18:26:05 +0300

    add new solution

--- Task056/Program.cs
+++ Task056/Program.cs
@@ -6,3 +6,62 @@
 // 8 4 2 4
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.Write("Введите размерность m массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность n массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] randomArray = new int[m, n];
+mas(m, n);
+Console.WriteLine("\nИсходный массив: ");
+printm(randomArray);
+int minSum = 1;
+int sum = SumLine(randomArray, 0);
+Console.WriteLine($"\nСтрока c наименьшей суммой элементов: {minSum}");
+
+void mas(int m, int n)
+{
+    int i, j;
+    Random rand = new Random();
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            randomArray[i, j] = rand.Next(1, 9);
+        }
+    }
+}
+
+void printm(int[,] array)
+{
+    int i, j;
+    for (i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int SumLine(int[,] array, int i)
+{
+    int sum = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sum += array[i, j];
+    }
+    return sum;
+}
+
+for (int i = 1; i < randomArray.GetLength(0); i++)
+{
+    if (sum > SumLine(randomArray, i))
+    {
+        sum = SumLine(randomArray, i);
+        minSum = i + 1;
+    }
+}
